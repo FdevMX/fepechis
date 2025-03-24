58fb4c08e8b0be8293339a87787210342fab5ca6
fdevmx <filesforcloud.fdev.mx@gmail.com>
2025-03-24 14:01:18 -0600

    Add initial assets and configuration files for various projects

---
+++ medinacomercio.com.mx/assets/inc/enviar_correo.php
@@ -0,0 +1,43 @@
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recibir datos del formulario
+    $nombre = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $telefono = htmlspecialchars($_POST['phone']);
+    $asunto1 = htmlspecialchars($_POST['asunto1']);
+    $mensaje = htmlspecialchars($_POST['message']);
+
+    // Configuraci贸n del correo
+    $para = "info@medinacomercio.com.mx";  // Aqu铆 va la direcci贸n de destino
+    $asunto = "$asunto1";
+    $cuerpo = "
+    <html>
+    <head>
+        <title>Nuevo mensaje de contacto</title>
+    </head>
+    <body>
+        <h2>Nuevo mensaje</h2>
+        <p><strong>Nombre:</strong> $nombre</p>
+        <p><strong>Email:</strong> $email</p>
+        <p><strong>Phone:</strong> $telefono</p>
+        <p><strong>Asunto:</strong> $asunto1</p>
+        <p><strong>Mensaje:</strong></p>
+        <p>$mensaje</p>
+    </body>
+    </html>
+    ";
+
+    // Para enviar correos HTML, debes configurar el encabezado Content-type
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= "From: $email" . "\r\n";
+
+    // Enviar el correo
+    if (mail($para, $asunto, $cuerpo, $headers)) {
+        echo "Correo enviado con exito.";
+    } else {
+        echo "Hubo un error al enviar el correo.";
+    }
+}
+?>
---
+++ velherweb.com.mx/enviar_correo.php
@@ -0,0 +1,54 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recibir datos del formulario
+    $nombre = htmlspecialchars($_POST['nombre']);
+    $email = htmlspecialchars($_POST['email']);
+     $asunto1 = htmlspecialchars($_POST['asunto1']);
+    $mensaje = htmlspecialchars($_POST['mensaje']);
+
+    // ConfiguraciÃ³n del correo
+    $para = "contacto@velherweb.com.mx";  // AquÃ­ va la direcciÃ³n de destino
+    $asunto = "$asunto1";
+    $cuerpo = "
+    <html>
+    <head>
+        <title>Nuevo mensaje de contacto</title>
+    </head>
+    <body>
+        <h2>Nuevo mensaje</h2>
+        <p><strong>Nombre:</strong> $nombre</p>
+        <p><strong>Email:</strong> $email</p>
+                <p><strong>Asunto:</strong> $asunto1</p>
+        <p><strong>Mensaje:</strong></p>
+        <p>$mensaje</p>
+    </body>
+    </html>
+    ";
+    
+    // Checamos si los campos no est¨¢n vac¨ªos, si s¨ª, respondemos.
+
+        if ( empty($nombre) OR empty($mensaje) OR empty($email) ) {
+
+            // Set a 400 (bad request) response code and exit.
+
+            http_response_code(400);
+
+            echo "Favor de llenar los campos correctamente.";
+
+            exit;
+
+        }
+
+    // Para enviar correos HTML, debes configurar el encabezado Content-type
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= "From: $email" . "\r\n";
+
+    // Enviar el correo
+    if (mail($para, $asunto, $cuerpo, $headers)) {
+        echo "Correo enviado exitosamente.";
+    } else {
+        echo "Hubo un error al enviar el correo.";
+    }
+}
+?>
